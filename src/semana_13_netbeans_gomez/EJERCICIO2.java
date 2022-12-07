40af539d91968936548e23403867badcc42e7938
LENOVO <LENOVO@DESKTOP-O01NVF0>
2022-12-06 19:57:50 -0500

    EJERCICIOS

---
+++ src/semana_13_netbeans_gomez/EJERCICIO2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semana_13_netbeans_gomez;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class EJERCICIO2 {
+    
+}
